c81cb8ea2b36c5a5fda9886060b485aa8e3adeba
rafkha <mrafkha@student.telkomuniversity.ac.id>
2023-03-19 23:12:44 +0800

    Add project files.

---
+++ MOD5_1302210115/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("NIM praktikan"); // ganti dengan nim praktikan
+        data.PrintData();
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+class program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Muhammad Rafkha Prima Aditya");
+    }
+
+
+}
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
+
